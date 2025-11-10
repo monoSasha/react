40448b53ba10c365bd170ead65124876def24cf0
Alexander <alexander.z@thewhitelabelagency.com>
2025-11-10 18:17:08 +0200

    test commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const answers = [];
+answers[0] = prompt('What is your name?', '');
+answers[1] = prompt('How old are you?', '');
+answers[2] = prompt('What is your last name?', '');
+
+
+console.log(`test${answers}`)
